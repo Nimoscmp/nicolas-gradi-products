df69265cf3f70232bc05b9e74d801a2c371175d8
Nimoscmp <nimos.cmp@gmail.com>
2021-09-03 21:51:54 -0500

    Prove hover effect to change product variant images

--- assets/productsgrid.js
+++ assets/productsgrid.js
@@ -1,73 +1,57 @@
-const select_number = document.getElementById('product_number_gradi');
-  const select_tags = document.getElementById('product_tag_gradi');
-  const cards = document.getElementsByClassName('product_card_gradi');
-  const cardsLength = cards.length;
+const pinkRadio = document.querySelectorAll('.gradidiv__product__radio--pink');
+const purpleRadio = document.querySelectorAll('.gradidiv__product__radio--purple');
+const orangeRadio = document.querySelectorAll('.gradidiv__product__radio--orange');
+const pinkImg = document.getElementsByClassName('gradidiv__product__img--pink');
+const purpleImg = document.getElementsByClassName('gradidiv__product__img--purple');
+const orangeImg = document.getElementsByClassName('gradidiv__product__img--orange');
 
-    /*select_number.addEventListener('change', event => {
+const pinkLength = pinkRadio.length;
+const purpleLength = purpleRadio.length;
+const orangeLength = orangeRadio.length;
 
-        for (let i = 0; i < cardsLength; i++) {
-            if(parseInt(event.target.value) <= i){
-                cards[i].style.display = 'none';
-            } else {
-                cards[i].style.display = 'block';
-            }
-        }
-    })*/
+for (let i = 0; i < pinkLength; i++) {
 
-  select_tags.addEventListener('change', function(event) {
+  const handleMouseLeave = () => {
+    pinkImg[i].style.display = 'block';
+    purpleImg[i].style.display = 'none';
+    orangeImg[i].style.display = 'none';
+    pinkRadio[i].style.borderColor = 'black';
+    purpleRadio[i].style.borderColor = 'lightgray';
+    orangeRadio[i].style.borderColor = 'lightgray';
+  }
 
-    let block = 0, none = 0;
-    let productsToShow = [];
+  pinkRadio[i].addEventListener('mouseenter', () => {
+    
+    pinkImg[i].style.display = 'block';
+    purpleImg[i].style.display = 'none';
+    orangeImg[i].style.display = 'none';
+    pinkRadio[i].style.borderColor = 'black';
 
-    for (let i = 0; i < cardsLength; i++) {
-      const tagChildNodes = cards[i].childNodes[7].childNodes;
-      const tagsLength = tagChildNodes.length;
-      let tag_exists = false;
-      
-      for (let j = 0; j < tagsLength; j++) {
-        if(tagChildNodes[j].nodeName === '#text'){
-          continue;
-        } 
-        
-        if(event.target.value === tagChildNodes[j].innerText){
-          tag_exists = true;
-        } 
-      }
-      if(event.target.value === 'all'){
-        tag_exists = true;
-      }
-      if(tag_exists){
-        productsToShow.push(cards[i]);
-      }
-
-      // if(tag_exists && (i < select_number.value)){
-      //   block++;
-      // } else {
-      //   none++;
-      // }
-      
-
-      if(tag_exists && (select_number.value)){
-        console.log("NB:" + block);
-        cards[i].style.display = 'block';
-      } else {
-        console.log("NN:" + none);
-        cards[i].style.display = 'none';
-      }
-    }
-
-    // const productsLength = productsToShow.length;
-
-    // for (let i = 0; i < productsLength; i++) {
-    //   if(tag_exists && (block <= select_number.value)){
-    //     console.log("NB:" + block);
-    //     cards[i].style.display = 'block';
-    //   } else {
-    //     console.log("NN:" + none);
-    //     cards[i].style.display = 'none';
-    //   }
-    // }
-
-
-    console.log("block:: " + block + "___" + "none:: " + none);
   })
+
+  pinkRadio[i].addEventListener('mouseleave', handleMouseLeave)
+  
+  purpleRadio[i].addEventListener('mouseenter', () => {
+    
+    pinkImg[i].style.display = 'none';
+    purpleImg[i].style.display = 'block';
+    orangeImg[i].style.display = 'none';
+    purpleRadio[i].style.borderColor = 'black';
+    pinkRadio[i].style.borderColor = 'lightgray';
+
+  })
+
+  purpleRadio[i].addEventListener('mouseleave', handleMouseLeave)
+  
+  orangeRadio[i].addEventListener('mouseenter', () => {
+    
+    pinkImg[i].style.display = 'none';
+    purpleImg[i].style.display = 'none';
+    orangeImg[i].style.display = 'block';
+    orangeRadio[i].style.borderColor = 'black';
+    pinkRadio[i].style.borderColor = 'lightgray';
+
+  })
+
+  orangeRadio[i].addEventListener('mouseleave', handleMouseLeave)
+}
